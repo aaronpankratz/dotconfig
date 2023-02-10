2a38888a9aa87b85c94c094160a611928d1cfda3
Aaron Pankratz <aaron.pankratz@affirm.com>
2023-02-10 11:04:22 -0600

    added lsp

--- nvim-lua/lua/plugins.lua
+++ nvim-lua/lua/plugins.lua
@@ -193,6 +193,20 @@ function M.setup()
       end,
     }
 
+    -- LSP
+    use {
+      "neovim/nvim-lspconfig",
+      opt = true,
+      event = "BufReadPre",
+      wants = { "nvim-lsp-installer" },
+      config = function()
+        require("config.lsp").setup()
+      end,
+      requires = {
+        "williamboman/nvim-lsp-installer",
+      },
+    }
+
     -- Bootstrap Neovim
     if packer_bootstrap then
       print "Restart Neovim required after installation!"
