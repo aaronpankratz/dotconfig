22f8d6c32896b4c5e0197a79ce135fd0d162bcbc
Aaron Pankratz <aaron@melia.capital>
2023-03-20 16:26:46 -0500

    Update init.lua

--- nvim-lua/init.lua
+++ nvim-lua/init.lua
@@ -1,1 +1,12 @@
 require("plugins").setup()
+
+-- Setup language servers.
+local lspconfig = require('lspconfig')
+lspconfig.pyright.setup {}
+lspconfig.tsserver.setup {}
+lspconfig.rust_analyzer.setup {
+  -- Server-specific settings. See `:help lspconfig-setup`
+  settings = {
+    ['rust-analyzer'] = {},
+  },
+}
